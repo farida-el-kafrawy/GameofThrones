468290c4fad3dd5d04eb61ea8bd10fdd86b65875
Mary Sogbanmu <marysogba@yahoo.com>
2021-03-06 14:17:45 +0000

    updated css

--- src/components/Feed/FeedComponent.jsx
+++ src/components/Feed/FeedComponent.jsx
@@ -1,12 +1,62 @@
 import React from 'react'
 import './FeedComponent.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
+const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 140,
+    },
+  });
+
+  
 // TODO - make sure FeedComponent is expecting the right props!
 export const FeedComponent = () => {
-    return <div className={'feed'}>
-        {/* TODO - add a placeholder for an empty feed */}
-        {/* TODO - build up a list of results */}
-        {/* TODO [STRETCH] - update this list to be a list/grid of STRETCH_Cards */}
-        I'm the feed component
+            
+  const classes = useStyles();
+
+  return (
+     <div>
+        <Card className={classes.root}>
+        <CardActionArea>
+            <CardMedia
+            className={classes.media}
+            image="/static/images/cards/contemplative-reptile.jpg"
+            title="Contemplative Reptile"
+            />
+            <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+                Lizard
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                across all continents except Antarctica
+            </Typography>
+            </CardContent>
+        </CardActionArea>
+        <CardActions>
+            <Button size="small" color="primary">
+            Share
+            </Button>
+            <Button size="small" color="primary">
+            Learn More
+            </Button>
+        </CardActions>
+       </Card>
+
+
     </div>
-};
+  );
+};   
+
+/*{ TODO - build up a list of results }*/
+        /*{ TODO [STRETCH] - update this list to be a list/grid of STRETCH_Cards }*/
--- src/components/Sidebar/SidebarComponent.jsx
+++ src/components/Sidebar/SidebarComponent.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './SidebarComponent.css';
+import { Button } from '@material-ui/core';
+
 
 
 // TODO - make sure SidebarComponent is expecting the right props!
@@ -11,30 +13,20 @@ export const SidebarComponent = () => {
         {
         /* TODO - flesh out this component to include all controls for configuring your data retrieval.
              This must include the category (books/characters/houses)
-             [STRETCH] Feel free to add anything else you want, for example:
-                * a dropdown to select number of items you want to retrieve
-                * a search bar to search for a particular item
-             You could even change what you can see in the search bar based on what you are searching for.
-             E.g add a dropdown to determine which field you're searching on ("name"/"title"/"alias")
         */} 
        
-        I'm the sidebar component
         <div id="mySidebar" class="sidebar">
-  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
   <form className="search-bar"> 
-        <input type="text" id="search" name="search" placeholder="Enter search..."/>
+        <input type="text" id="search" name="search" placeholder="Enter search..."/> <br></br><br></br>
+        <Button variant="contained" color="secondary">
+                  SEARCH
+         </Button>
+         
         </form>
   <a href="#">Home</a>
   <a href="#">Houses</a>
   <a href="#">Books</a>
   <a href="#">Characters</a>
-  
-</div>
-
-<div id="main">
-  <button class="openbtn" onclick="openNav()">&#9776; Open Sidebar</button>
-  <h2>Collapsed Sidebar</h2>
-  <p>Content...</p>
 </div>
 
 
