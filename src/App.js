f5296fabf5d84272c60eba28ce26c872302b0412
Lionel Smith <smithlionel1121@gmail.com>
2021-03-10 01:14:12 +0000

    Data Choices added

--- src/App.js
+++ src/App.js
@@ -1,71 +1,73 @@
-import './App.css';
-import React, {useState, useEffect} from 'react';
-import { HeaderComponent } from './components/Header/HeaderComponent';
-import { SidebarComponent } from './components/Sidebar/SidebarComponent';
-import { FeedComponent } from './components/Feed/FeedComponent';
-import axios from 'axios';
-
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { HeaderComponent } from "./components/Header/HeaderComponent";
+import { SidebarComponent } from "./components/Sidebar/SidebarComponent";
+import { FeedComponent } from "./components/Feed/FeedComponent";
+import axios from "axios";
 
 function App() {
+  // TODO - this is the "main" component for our app, and it will include all the global state that we care about
+  //  This should include things like:
+  //  * the sidebar expanded state
+  //  * the selected category (books/characters/houses)
+  //  * the feed results
 
-    // TODO - this is the "main" component for our app, and it will include all the global state that we care about
-    //  This should include things like:
-    //  * the sidebar expanded state
-    //  * the selected category (books/characters/houses)
-    //  * the feed results
+  // TODO [STRETCH] - This could also include
+  //  * the search term (if there is one)
+  //  * whether the app is fetching data (loading)
+  //  * any additional filters (number of results/filter terms in query string)
 
-    // TODO [STRETCH] - This could also include
-    //  * the search term (if there is one)
-    //  * whether the app is fetching data (loading)
-    //  * any additional filters (number of results/filter terms in query string)
+  //  Each part of the state will need to be passed down to its respective component(s) in order for it
+  //  to be displayed/updated in the correct part of the UI
+  //  * E.g. maybe you can expand/collapse the sidebar from the header and the feed, as well as the sidebar itself
+  //  This means that the state will need to be accessed/updated from all of these components!
 
-    //  Each part of the state will need to be passed down to its respective component(s) in order for it
-    //  to be displayed/updated in the correct part of the UI
-    //  * E.g. maybe you can expand/collapse the sidebar from the header and the feed, as well as the sidebar itself
-    //  This means that the state will need to be accessed/updated from all of these components!
+  // To get started:
+  // TODO - add in an expanded state/setState
+  // TODO - add in a feedResults state/setState
+  // (See cheat sheet for useState example)
 
-    // To get started:
-    // TODO - add in an expanded state/setState
-    // TODO - add in a feedResults state/setState
-    // (See cheat sheet for useState example)
+  // TODO - import getData() from api (you will need to write this function)
+  //         and call it here (setting the results to the feedResults)
+  // TODO [STRETCH] - implement loading state and pass to FeedComponent
 
+  // TODO - pass in expanded sidebar state to components that need to know about it/update it.
 
-    // TODO - import getData() from api (you will need to write this function)
-    //         and call it here (setting the results to the feedResults)
-    // TODO [STRETCH] - implement loading state and pass to FeedComponent
+  const [feedResults, setFeedResults] = useState([]);
+  const [dataChoice, setDataChoice] = useState("houses");
 
-    // TODO - pass in expanded sidebar state to components that need to know about it/update it.
+  function fetchData(selection) {
+    const axios = require("axios");
 
-    const [houses, setHouses] = useState([])
-    function fetchHouses (){
-        const axios = require('axios');
- 
-    
-        // Make a request for a user with a given ID
-        axios.get('https://www.anapioficeandfire.com/api/houses')
-          .then(function (response) {
-            // handle success
-        setHouses(response.data)
-        
-          })
-          .catch(function (error) {
-            // handle error
-            console.log(error);
-          })
-    };
+    // Make a request for a user with a given ID
+    axios
+      .get(`https://www.anapioficeandfire.com/api/${selection}`)
+      .then(function (response) {
+        // handle success
+        setFeedResults(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  }
 
-   useEffect (fetchHouses, [])
-   
-   console.log(houses)
-    return (
+  useEffect(() => {
+    fetchData(dataChoice);
+  }, [dataChoice]);
 
-        <div className="app">
-            <HeaderComponent />
-            <img src={require('../src/images/got.jpg').default} height="232" width="430"/>
-            <SidebarComponent />
-            <FeedComponent houses={houses} />
-        </div>
-    );
+  return (
+    <div className="app">
+      <HeaderComponent />
+      <img
+        src={require("../src/images/got.jpg").default}
+        height="232"
+        width="430"
+      />
+      <SidebarComponent setDataChoice={setDataChoice} dataChoice={dataChoice} />
+      <FeedComponent feedResults={feedResults} />
+    </div>
+  );
 }
 
 export default App;
